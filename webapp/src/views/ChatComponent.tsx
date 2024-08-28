99b569dfe8a20b38cd4ab160f2d04e0418f0c566
Taban Cosmos <cosmostaban@gmail.com>
2024-08-28 13:05:40 -0700

    Minor update

--- webapp/src/views/ChatComponent.tsx
+++ webapp/src/views/ChatComponent.tsx
@@ -77,7 +77,7 @@ const ChatComponent: React.FC = () => {
     const messagesEndRef = useRef<null | HTMLDivElement>(null);
     const abortControllerRef = useRef<AbortController | null>(null);
     const [selectedChatService, setSelectedChatService] = useState<'ollama' | 'bedrock'>('ollama');
-    const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     const syntaxStyle: SyntaxHighlighterProps['style'] | CSSProperties = oneDark;
 
 
@@ -282,24 +282,13 @@ const ChatComponent: React.FC = () => {
                         <div className={`text-black md:w-[900px] p-4  ${messages.length > 0 ? 'hidden' : 'block'} flex flex-col justify-center my-auto mx-auto w-full font-semibold text-2xl mt-10`}>
                             {/* Section for Executives */}
                             <div className="text-black font-medium text-2xl mb-4">For Executive</div>
-                            <div className="flex flex-col md:flex-row gap-4 w-full justify-between font-bold text-lg">
-                                <div className="w-full p-4 md:p-10 shadow flex flex-col justify-around items-center rounded-[10px] border border-black h-[102px]">
-                                    <div className="text-black text-sm text-center">Market Trends and Competitive Analysis</div>
-                                </div>
-                                <div className="w-full p-4 md:p-10 shadow flex flex-col justify-around items-center rounded-[10px] border border-black h-[102px]">
-                                    <div className="text-black text-sm text-center">Strategic Goals and Business Objectives</div>
-                                </div>
-                                <div className="w-full p-4 md:p-10 shadow flex flex-col justify-around items-center rounded-[10px] border border-black h-[102px]">
-                                    <div className="text-black text-sm text-center">Organizational Structure and Key Team Profiles</div>
-                                </div>
-                            </div>
-
                             {/* Section for Employees */}
-                            <div className="text-black font-medium text-2xl mt-6 mb-4">For Employees</div>
+                            {/* <div className="text-black font-medium text-2xl mt-6 mb-4">For Employees</div> */}
+
                             <div className="flex flex-col md:flex-row gap-4 w-full justify-around font-bold ">
                                 <button
                                     className="w-full p-4 md:p-10  text-2xl shadow flex flex-col justify-around items-center rounded-[10px] border border-black h-[102px]"
-                                    onClick={() => handleSubmitCustom('Market Trends and Competitive Analysis')}
+                                    onClick={() => handleSubmitCustom('Market Trends and Competitive Analysis: AI use case in organization lost knowledge due to employee getting fired or quitting and its impact on long growth to the organzation')}
                                 >
                                     <div className="text-black text-sm text-center">Market Trends and Competitive Analysis</div>
                                 </button>
